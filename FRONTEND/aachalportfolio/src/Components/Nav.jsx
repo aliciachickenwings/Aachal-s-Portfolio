e3eb6109bf705467dd57311c48675b7c6f90ac3d
aliciachickenwings <91463251+AachalShrestha@users.noreply.github.com>
2024-08-04 10:19:07 +0200

    FE: Navigation animation

--- FRONTEND/aachalportfolio/src/Components/Nav.jsx
+++ FRONTEND/aachalportfolio/src/Components/Nav.jsx
@@ -1,23 +1,41 @@
 import "../Styles/Nav.css";
+import { useRef, useEffect, useState } from "react";
 
 function Nav() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleNavbar = () => {
+    console.log("clicked on toggle");
+    setIsActive(!isActive);
+  };
+
   return (
     <div className="nav-wrapper">
-      <a href="/">
-        <p>HOME</p>
-      </a>
-      <a href="/aboutme">
-        <p>ABOUT ME</p>
-      </a>
-      <a href="/work">
-        <p>WORK</p>
-      </a>
-      <a href="/archive">
-        <p>ARCHIVE</p>
-      </a>
-      <a href="/contact">
-        <p>CONTACT</p>
-      </a>
+      <div
+        className={`toggle-button ${isActive ? "active" : ""}`}
+        onClick={toggleNavbar}
+      >
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      <div className={`nav-elements ${isActive ? "active" : ""}`}>
+        <a href="/">
+          <p>HOME</p>
+        </a>
+        <a href="/aboutme">
+          <p>ABOUT ME</p>
+        </a>
+        <a href="/work">
+          <p>WORK</p>
+        </a>
+        <a href="/archive">
+          <p>ARCHIVE</p>
+        </a>
+        <a href="/contact">
+          <p>CONTACT</p>
+        </a>
+      </div>
     </div>
   );
 }
